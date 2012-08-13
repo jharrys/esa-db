1fad9312307ab1f903abce751cb64a4cd8845a21
Stuart Simpson <stuart.simpson@imail.org>
2012-08-13 17:12:57 -0600

    This is the initial load of seed data for the EISA Exception Form.

---
+++ DatabaseModel/src/form_seed_data.sql
@@ -0,0 +1,2 @@
+REM INSERTING into FORM
+Insert into FORM (ID,NAME,DESCRIPTION,DATE_CREATED,CREATED_BY,LAST_UPDATED,UPDATED_BY) values (1,'EISA Exception','This document is used to apply for an exception to the supported standard.',to_timestamp('13-AUG-12','DD-MON-RR HH.MI.SSXFF AM'),'bulkLoad',to_timestamp('13-AUG-12','DD-MON-RR HH.MI.SSXFF AM'),'bulkLoad');
