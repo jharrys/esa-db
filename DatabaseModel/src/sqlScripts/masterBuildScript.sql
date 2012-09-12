d8c7275734ecef0ba123ed17396988199f1e205e
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-09-11 18:33:13 -0600

    Added authentication and authorization tables.  And other refactors.
    Refactored master script.
    Refactored item catalog seed data.
    Removed the catalog configuration cross-reference - too complex to keep up by hand.

--- DatabaseModel/src/sqlScripts/masterBuildScript.sql
+++ DatabaseModel/src/sqlScripts/masterBuildScript.sql
@@ -1,7 +1,9 @@
 @clean_esa.sql
 @build_esa.sql
+@esa_aa.sql
 @lookup_list_seed_data.sql
 @lookup_element_seed_data.sql
 @form_seed_data.sql
 @form_field_seed_data.sql
 @item_catalog_seed_data_from_eisa_excel.sql
+@party_seed_data.sql
