14f6101f5397919aa35cc66b692b7bd9f02d495b
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-09-18 17:27:31 -0600

    Modified some seed data and fixed the esa_user table and renamed one seed data file.

--- DatabaseModel/src/sqlScripts/masterBuildScript.sql
+++ DatabaseModel/src/sqlScripts/masterBuildScript.sql
@@ -5,6 +5,6 @@
 @lookup_element_seed_data.sql
 @form_seed_data.sql
 @form_field_seed_data.sql
-@item_catalog_seed_data_from_eisa_excel.sql
+@item_seed_data_from_eisa_excel.sql
 @party_seed_data.sql
 @sequence_update.sql
