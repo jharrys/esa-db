5cd3aec8faf129942cea189725fc9cb17623c3ae
John Harris on D91M31M1/Windows 7 <john.harris@imail.org>
2012-09-24 23:40:01 -0600

    Bug fixes with sequence updater and item seeder.

--- DatabaseModel/src/sqlScripts/sequence_update.sql
+++ DatabaseModel/src/sqlScripts/sequence_update.sql
@@ -16,6 +16,7 @@ declare
     TYPE generic_cursor_type IS REF CURSOR;
     max_id number;
     next_id number;
+    curr_id number;
     c2 generic_cursor_type;
 begin
     for r1 in c1 loop
@@ -23,14 +24,21 @@ begin
         fetch c2 into max_id;
         close c2;
         
+        -- initialize nextval
+        execute immediate 'select '||r1.sequence_name||'.nextval from dual' into next_id;
+        
         if( max_id != 0 ) then
-            dbms_output.put_line( 'seq name = '||r1.sequence_name );
-            execute immediate 'alter sequence '||r1.sequence_name||' increment by '||to_char(max_id);
-            dbms_output.put_line( 'max_id = '||to_char(max_id) );
-            execute immediate 'select '||r1.sequence_name||'.nextval from dual' into next_id;
-            dbms_output.put_line( 'sequence value = '||to_char(next_id) );
-            execute immediate 'alter sequence '||r1.sequence_name||' increment by 1';
-            dbms_output.put_line( 'sequence: '||r1.sequence_name||' is at '||to_char(max_id+1) );
+            execute immediate 'select '||r1.sequence_name||'.currval from dual' into curr_id;
+            if ( curr_id < max_id ) then
+              dbms_output.put_line( 'seq name = '||r1.sequence_name );
+              dbms_output.put_line( 'seq name curr-val = '||to_char(curr_id) );
+              execute immediate 'alter sequence '||r1.sequence_name||' increment by '||to_char(max_id);
+              dbms_output.put_line( 'max_id = '||to_char(max_id) );
+              execute immediate 'select '||r1.sequence_name||'.nextval from dual' into next_id;
+              dbms_output.put_line( 'sequence value = '||to_char(next_id) );
+              execute immediate 'alter sequence '||r1.sequence_name||' increment by 1';
+              dbms_output.put_line( 'sequence: '||r1.sequence_name||' is at '||to_char(max_id+1) );
+            end if;
         end if;
     end loop;
 end;
